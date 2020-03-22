2d2f97f60f14e730981ee11ba5654d4e2a006e93
hinata98 <taipt1998@gmail.com>
2020-03-22 18:47:10 +0700

    make it better

--- src/main/java/com/aht/NhanSu/controller/HelloWord.java
+++ src/main/java/com/aht/NhanSu/controller/HelloWord.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/rest/helo")
 public class HelloWord {
     @GetMapping("")
     public ResponseEntity xinChao(){
